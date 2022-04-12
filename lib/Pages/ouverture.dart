b9d82d5b1e5c61dac739521c305fdfd36a0d626d
Harold Dansou <dansouscareface@gmail.com>
2022-04-12 16:56:23 +0200

    openingpage

--- lib/Pages/ouverture.dart
+++ lib/Pages/ouverture.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:projet_tetudes/main.dart';
+import 'package:projet_tetudes/Pages/home_page.dart';
+
+const owlUrl =
+    'https://www.univ-grenoble-alpes.fr/medias/photo/uga-soutien-etudiants_1611570121559-jpg';
 
 class Ouverture extends StatefulWidget {
   const Ouverture({Key? key}) : super(key: key);
@@ -11,21 +14,35 @@ class Ouverture extends StatefulWidget {
 class _OuvertureState extends State<Ouverture> {
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text(
-        'T`Etudes, For Strangers by Strangers',
-        textAlign: TextAlign.center,
-      ),
-    );
+    return Column(children: <Widget>[
+      Image.network(owlUrl),
+      TextButton(
+        child: const Text('T`Etudes, For Strangers by Strangers',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              color: Colors.lightBlueAccent,
+              fontSize: 16.0,
+              fontWeight: FontWeight.bold,
+              letterSpacing: 0.15,
+            )),
+        onPressed: (() => {HomePage}),
+      )
+    ]);
   }
 }
 
-class TEtudes extends StatelessWidget {
-  const TEtudes({Key? key}) : super(key: key);
+class Open extends StatelessWidget {
+  const Open({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return const MaterialApp(
+      home: Scaffold(
+        backgroundColor: Color.fromARGB(255, 37, 24, 150),
+        body: Center(
+          child: Ouverture(),
+        ),
+      ),
+    );
   }
 }
